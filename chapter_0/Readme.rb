0a3cbcadb0f9354bcc141e2f617273dbe050dc9c
Nick Vass <prkarpi@hotmail.com>
2016-02-21 17:33:33 -0700

    Add Readme.rb
    [#113796359]

---
+++ chapter_0/Readme.rb
@@ -0,0 +1,12 @@
+# Output from my iTerm
+
+Vassio-Book:lrthw_exercises $ ruby -v
+ruby 2.2.4p230 (2015-12-16 revision 53155) [x86_64-darwin15]
+
+(113796355_intro_chapter_master) Nick Vass
+Vassio-Book:lrthw_exercises $ mkdir chapter_0
+
+(113796355_intro_chapter_master) Nick Vass
+Vassio-Book:chapter_0 $ ls
+Readme.md
+
