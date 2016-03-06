baf733d07c676cbff9cd490ae3ed6691123bb91e
Nick Vass <prkarpi@hotmail.com>
2016-03-06 15:35:46 -0700

    homewrok chapter 3: lrthw
    [#113796365]

--- chapter_3/ex_3.rb
+++ chapter_3/ex_3.rb
@@ -1,41 +1,40 @@
 #!/usr/bin/env ruby
 
 # prints the sentence
-puts "I will now count my chickens:"
+puts 'I will now count my chickens:'
 
-#calculates the value of given numbers 30 / 6 + 5 (in this order)
+# calculates the value of given numbers 30 / 6 + 5 (in this order)
 puts "Hens #{25.9 + 30.4 / 6.7}"
 
-#gives you a number for the roosters.
+# gives you a number for the roosters.
 puts "Roosters #{100.3 - 25.6 * 3.1 % 4.0}"
 
-#prints the line of text
-puts "Now I will count the eggs:"
+# prints the line of text
+puts 'Now I will count the eggs:'
 
-#calculates the value with numbers.
-puts "#{3.9 + 2.3 + 1.8 - 5.0 + 4.4 % 2.2 - 1.4 / 4.9 + 6.7}"
+# calculates the value with numbers.
+puts { 3.9 + 2.3 + 1.8 - 5.0 + 4.4 % 2.2 - 1.4 / 4.9 + 6.7 }
 
 # prints the line without calculations
-puts "Is it true that 3.3 + 2.2 < 5.5 - 7.7?"
+puts 'Is it true that 3.3 + 2.2 < 5.5 - 7.7?'
 
-#puts the given calculation with a less thn sign
+# puts the given calculation with a less thn sign
 puts 3.3 + 2.2 < 5.5 - 7.7
 
 # prints what's inside the doulbe quotes and puts the iteration
 puts "What is 3.3 + 2.2? #{3.3 + 2.2}"
 puts "What is 5.5 - 7.7? #{5.5 - 7.7}"
 
-#prints the line
-puts "Oh, that's why it's false."
+# prints the line
+puts 'Oh, that\'s why it\'s false.'
 
-#prints the line
-puts "How about some more."
+# prints the line
+puts 'How about some more.'
 
-#prints the lines with double quotes and puts the lines with iterations using true or false statements since less than and greated than signs are used
+# rubocop does not like long lines
 puts "Is it greater? #{5.3 > -2.1}"
 puts "Is it greater or equal? #{5.8 >= -2.5}"
 puts "Is it less or equal? #{5.4 <= -2.9}"
-
 
 # + plus = it add the numbers
 # - minus = minus sign used to calculate the value
